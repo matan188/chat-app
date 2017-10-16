4aef39cb29132180b3cefc55d72ef8442abf47d1
Matan <matan.modai@gmail.com>
2017-10-16 11:09:34 +0300

    init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express(); 
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.send('index.html');
+});
+
+
+app.listen(port, () => {
+    console.log("listening to 3000");
+});
